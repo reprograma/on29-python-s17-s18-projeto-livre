2ee52eba4f96405c9b3ff6d437083960a9480924
lucianacarvl <luccarvl@gmail.com>
2024-08-02 20:28:14 -0300

    Update

---
+++ exercicios/para-casa/base_final_nordeste.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df_estacoes_nordeste = pd.read_csv('estacoes_nordeste.csv')
+df_base_final = pd.read_csv('base_final_tratada.csv')
+
+codigos_estacao_nordeste = df_estacoes_nordeste['Estacao'].unique()
+df_base_final_nordeste = df_base_final[df_base_final['Codigo_Estacao'].isin(codigos_estacao_nordeste)]
+
+df_base_final_nordeste.to_csv('base_final_nordeste.csv', index=False)
+
+print(df_base_final_nordeste.head())
